87732e9e03ee1b9a82231c9916334a5ec42343a2
Амир Абдрафиков <sequze@mail.ru>
2025-03-25 23:05:12 +0300

    create config and db_helper

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from db_helper import db_helper
 
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     async with db_helper.engine.begin() as conn:
+#         # await conn.run_sync(Base.metadata.create_all)
+#     yield
 
 app = FastAPI()
